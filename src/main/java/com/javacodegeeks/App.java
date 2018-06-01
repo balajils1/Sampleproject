40ef86d2c88d32c4318b82b49d2b7a6c49aff0be
balajils1 <39579627+balajils1@users.noreply.github.com>
2018-06-01 09:41:50 +0530

    Update App.java

--- src/main/java/com/javacodegeeks/App.java
+++ src/main/java/com/javacodegeeks/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helloooo Jfrogger svoga 2018!" );
+        System.out.println( "Hello voyagers Jfrog 2018!" );
     }
 }
