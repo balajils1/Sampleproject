3a1ce922d8bd58a4701a940819ea2d74b3ec9e0c
balajils1 <39579627+balajils1@users.noreply.github.com>
2019-06-26 22:58:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var _ = require('underscore');
+
+module.exports = {
+  sayHello: sayHello
+};
+
+function sayHello() {
+  var words = ['Hello', 'World!'];
+  var message = '';
+  _.each(words, function(word) {
+    message += word + ' ';
+  });
+  return message;
+}
